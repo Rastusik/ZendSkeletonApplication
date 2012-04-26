bd46139192f73e9c2212278316185bbfc19cd9db
Marco Pivetta <ocramius@gmail.com>
2012-04-26 14:09:22 +0200

    Adding example controller used for tests

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -7,8 +7,24 @@ use Zend\Mvc\Controller\ActionController,
 
 class IndexController extends ActionController
 {
+
+    public function __construct(\Zend\Mvc\Controller\PluginBroker $broker)
+    {
+
+    }
+
     public function indexAction()
     {
+        if(!$this->locator instanceof \Zend\Di\Di) {
+            throw new \BadMethodCallException('No locator of type Zend\Di\Di provided!');
+        }
+        $dumper = new \Zend\Di\Instance\Dumper($this->locator);
+        var_dump($dumper->getInitialInstanceDefinitions());
+        var_dump($dumper->getInjectedDefinitions(__CLASS__));
+
+
+        die();
+
         return new ViewModel();
     }
 }
